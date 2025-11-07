aa8cd9f1480c0ce5c98c85dec04d468fd78e9085
Titov Ilya <titov1608@gmail.com>
2025-11-07 03:13:35 +0300

    init

---
+++ src/main/java/pro/sing/demojavacorcoursework/DemoJavacorCourseworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sing.demojavacorcoursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavacorCourseworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJavacorCourseworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sing/demojavacorcoursework/DemoJavacorCourseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sing.demojavacorcoursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavacorCourseworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
